099ca1e53f5166ccd8ced5ae53bc3356e53d300b
Archie <wonkaba@gmail.com>
2017-07-20 07:56:07 -0700

    started

---
+++ src/assignments/Test.java
@@ -0,0 +1,13 @@
+package assignments;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String x = "hello";
+		String y = "world";
+		
+		System.out.println(x+" "+y);
+
+	}
+
+}
